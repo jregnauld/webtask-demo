5e70b75e07b0d9f10e9ae2de548c96a227dfed33
Julien <j.regnauld@gmail.com>
2016-07-20 22:15:16 +0200

    fixing pod for unit testing

---
+++ webtask-demo/webtask-demoTests/ApplicationUserTests.swift
@@ -0,0 +1,17 @@
+//
+//  ApplicationUserTests.swift
+//  webtask-demo
+//
+//  Created by Julien Regnauld on 7/20/16.
+//  Copyright © 2016 Julien Regnauld. All rights reserved.
+//
+
+import Foundation
+import Quick
+import Nimble
+
+class ApplicationUserTests: QuickSpec {
+  override func spec() {
+    
+  }
+}
